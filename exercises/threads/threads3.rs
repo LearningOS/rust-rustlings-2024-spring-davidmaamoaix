646d291f680e02a220d242a9e9b3fcc65ce6bd02
davidmaamoaix <davidmaamoaix@gmail.com>
2024-04-08 11:10:48 -0400

    fixes

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -3,6 +3,8 @@
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
 
+// I AM NOT DONE
+
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::thread;
@@ -28,13 +30,11 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     let qc = Arc::new(q);
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
-    let tx1 = tx.clone();
-    let tx2 = tx.clone();
 
     thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx1.send(*val).unwrap();
+            tx.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
@@ -42,13 +42,12 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     thread::spawn(move || {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx2.send(*val).unwrap();
+            tx.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 }
 
-#[test]
 fn main() {
     let (tx, rx) = mpsc::channel();
     let queue = Queue::new();
